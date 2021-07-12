c220b22796cacf22f8dc9e6dc8131ad8c8a9cc9f
Amanda April <amanda_april_28rpl@student.smktelkom-mlg.sch.id>
2021-07-12 13:56:40 +0700

    fead(update): about

--- frontend-tourin/pages/about.vue
+++ frontend-tourin/pages/about.vue
@@ -1,7 +1,35 @@
 <template>
-  <h1>About us</h1>
+  <section class="recipes">
+      <article class="recipe">
+       <div></div>
+       <h1>Title</h1>
+        <p>Some nice preview text</p>
+      </article>
+      <article class="recipe">
+       <div></div>
+       <h1>Title</h1>
+        <p>Some nice preview text</p>
+      </article>
+  </section>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+.recipes{
+   display: flex;
+   flex-flow: row wrap; 
+}
+
+.recipe{
+    box-sizing: border-box;
+    width: 280px;
+    padding: 8px;
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 2px #aaa;
+}
+</style>>
+
+
